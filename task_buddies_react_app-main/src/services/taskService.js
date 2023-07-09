418539e23d53e3b1a215c5d02d71c254a392048a
Enzo SALVATI - babi <95984938+babise@users.noreply.github.com>
2023-07-09 15:06:44 +0200

    Add Validated task et disable if selectedDate is not today

--- task_buddies_react_app-main/src/services/taskService.js
+++ task_buddies_react_app-main/src/services/taskService.js
@@ -2,79 +2,120 @@ const API_BASE_URL = process.env.API_URL;
 const token = localStorage.getItem("token");
 
 export const taskService = {
-    async fetchTasksByDate(selectedDate) {
-        try {
-            const response = await fetch(
-                `${API_BASE_URL}/task/date?date=${selectedDate.toISOString()}`, {
-                    headers: {
-                        "Content-Type": "application/json",
-                        Authorization: `Bearer ${token}`,
-                    },
-                }
-            );
+	async fetchTasksByDate(selectedDate) {
+		try {
+			const response = await fetch(
+				`${API_BASE_URL}/task/date?date=${selectedDate.toISOString()}`,
+				{
+					headers: {
+						"Content-Type": "application/json",
+						Authorization: `Bearer ${token}`,
+					},
+				}
+			);
 
-            if (response.ok) {
-                const tasks = await response.json();
-                return tasks.map((task) => ({
-                    id: task.id,
-                    title: task.title,
-                    // Map other properties as needed
-                }));
-            } else {
-                throw new Error("Failed to load tasks");
-            }
-        } catch (error) {
-            throw new Error("Failed to load tasks");
-        }
-    },
+			if (response.ok) {
+				const tasks = await response.json();
+				return tasks.map((task) => ({
+					id: task.id,
+					title: task.title,
+					// Map other properties as needed
+				}));
+			} else {
+				throw new Error("Failed to load tasks");
+			}
+		} catch (error) {
+			throw new Error("Failed to load tasks");
+		}
+	},
 
-    async confirmTask(id) {
-        try {
-            const response = await fetch(`${API_BASE_URL}/task-user`, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: `Bearer ${token}`,
-                },
-                body: JSON.stringify({
-                    task: {
-                        id: id
-                    }
-                }),
-            });
+	async addTask(title, recurrences) {
+		try {
+			const response = await fetch(`${API_BASE_URL}/task`, {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: `Bearer ${token}`,
+				},
+				body: JSON.stringify({
+					title,
+					recurrences,
+				}),
+			});
 
-            if (response.ok) {
-                // Task added successfully
-            } else {
-                throw new Error("Failed to add taskUser");
-            }
-        } catch (error) {
-            throw new Error("Failed to add taskUser");
-        }
-    },
+			if (response.ok) {
+				// Task added successfully
+			} else {
+				throw new Error("Failed to add task");
+			}
+		} catch (error) {
+			throw new Error("Failed to add task");
+		}
+	},
+	async addTaskUser(taskId) {
+		try {
+			const response = await fetch(`${API_BASE_URL}/task-user`, {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: `Bearer ${token}`,
+				},
+				body: JSON.stringify({
+					task: {
+						id: taskId,
+					},
+				}),
+			});
 
+			if (response.ok) {
+				// Task user added successfully
+			} else {
+				throw new Error("Failed to add task user");
+			}
+		} catch (error) {
+			throw new Error("Failed to add task user");
+		}
+	},
 
-    async addTask(title, recurrences) {
-        try {
-            const response = await fetch(`${API_BASE_URL}/task`, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: `Bearer ${token}`,
-                },
-                body: JSON.stringify({
-                    title,
-                    recurrences,
-                }),
-            });
+	async removeTaskUser(taskId) {
+		try {
+			const response = await fetch(`${API_BASE_URL}/task-user/${taskId}`, {
+				method: "DELETE",
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: `Bearer ${token}`,
+				},
+			});
 
-            if (response.ok) {
-                // Task added successfully
-            } else {
-                throw new Error("Failed to add task");
-            }
-        } catch (error) {
-            throw new Error("Failed to add task");
-        }
-    },
+			if (response.ok) {
+				// Task user removed successfully
+			} else {
+				throw new Error("Failed to remove task user");
+			}
+		} catch (error) {
+			throw new Error("Failed to remove task user");
+		}
+	},
+	async hasTaskBeenValidatedOnDate(taskId, onDate) {
+		try {
+			const response = await fetch(
+				`${API_BASE_URL}/task-user/${taskId}/validated-today/${onDate}`,
+				{
+					headers: {
+						"Content-Type": "application/json",
+						Authorization: `Bearer ${token}`,
+					},
+				}
+			);
+
+			if (response.ok) {
+				const result = await response.json();
+				return result.validated;
+			} else {
+				throw new Error("Failed to check task validation");
+			}
+		} catch (error) {
+			throw new Error("Failed to check task validation");
+		}
+	},
 };
