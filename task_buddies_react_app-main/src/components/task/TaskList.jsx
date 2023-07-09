41984cef3fc5cfdf4a1a31ac988f4cd4d9f78e30
Bruce MONGTHE <58480180+BruceMong@users.noreply.github.com>
2023-07-09 19:53:58 +0200

    Redux

--- task_buddies_react_app-main/src/components/task/TaskList.jsx
+++ task_buddies_react_app-main/src/components/task/TaskList.jsx
@@ -1,57 +1,76 @@
-// TaskList.jsx
 import React, { useEffect, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { taskService } from "../../services/taskService";
 import TaskTile from "./TaskTile";
-import "../../styles/task.scss";
+
+import { useSelector, useDispatch } from "react-redux";
+import { fetchTasks, taskSliceActions } from "../../store/dashboard/task"; 
 
 const TaskList = () => {
-	const [tasks, setTasks] = useState([]);
-	const [selectedDate, setSelectedDate] = useState(new Date());
+  //const [tasks, setTasks] = useState([]);
 
-	useEffect(() => {
+  const dispatch = useDispatch();
+  const { tasks, status, error } = useSelector((state) => state.task);
+  const selectedDateStr = useSelector((state) => state.task.selectedDate);
+  const selectedDate = new Date(selectedDateStr);
+  console.log(selectedDate);
+
+  // Utilisez useEffect pour déclencher le fetch des tâches lorsque le composant est monté.
+  useEffect(() => {
+    dispatch(fetchTasks());
+  }, [dispatch]);
+
+  // Lorsque vous voulez modifier la date sélectionnée, convertissez la nouvelle Date en chaîne de caractères ISO.
+  const handleDateChange = (newDate) => {
+    dispatch(taskSliceActions.setSelectedDate(newDate.toISOString()));
+  };
+
+  // Si le status est 'loading', nous affichons un indicateur de chargement.
+  if (status === "loading") {
+    return <div>Chargement...</div>;
+  }
+
+  // Si une erreur s'est produite, nous l'affichons.
+  if (error) {
+    return <div>Erreur : {error}</div>;
+  }
+
+  /* 	useEffect(() => {
 		fetchTasks();
 	}, [selectedDate]);
 
 	const fetchTasks = async () => {
 		try {
-			const fetchedTasks = await taskService.fetchTasksByDate(selectedDate);
-			const tasksWithValidation = await Promise.all(
-				fetchedTasks.map(async (task) => {
-					const validated = await taskService.hasTaskBeenValidatedOnDate(
-						task.id,
-						selectedDate
-					);
-					return { ...task, validated };
-				})
+			const fetchedTasks = await taskService.fetchTasksByDate(
+				selectedDate,
+				token
 			);
-			setTasks(tasksWithValidation);
+			setTasks(fetchedTasks);
 		} catch (error) {
 			console.error("Error fetching tasks:", error);
 		}
-	};
+	}; */
 
-	return (
-		<div className="componentContainer">
-			<div className="componentHeader">
-				<p>Tâches à réaliser 💪</p>
-			</div>
-			<DatePicker
-				selected={selectedDate}
-				onChange={(date) => setSelectedDate(date)}
-			/>
+  return (
+    <div className="componentContainer">
+      <div className="componentHeader">
+        <p>Tâches à réaliser 💪</p>
+      </div>
+      <DatePicker
+        selected={selectedDate}
+        onChange={(date) => handleDateChange(date)}
+      />
 
-			{tasks.map((task) => (
-				<TaskTile
-					key={task.id}
-					task={task}
-					fetchTasks={fetchTasks}
-					selectedDate={selectedDate}
-				/>
-			))}
-		</div>
-	);
+      {tasks.map((task) => (
+        <TaskTile
+          key={task.id}
+          task={task}
+          fetchTasks={fetchTasks}
+          selectedDate={selectedDate}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default TaskList;
--- task_buddies_react_app-main/src/store/dashboard/task.js
+++ task_buddies_react_app-main/src/store/dashboard/task.js
@@ -1,14 +1,55 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { taskService } from "../../services/taskService"; // Assurez-vous que ce chemin d'importation est correct.
+// L'action asynchrone qui fait le fetch.
+export const fetchTasks = createAsyncThunk(
+  "task/fetchTasks",
+  async (_, { getState, rejectWithValue }) => {
+    // Remplacez selectedDate par _
+    const selectedDate = getState().task.selectedDate; // Accédez à selectedDate à partir de l'état courant
+    try {
+      const fetchedTasks = await taskService.fetchTasksByDate(new Date(selectedDate));
+      return fetchedTasks;
+    } catch (err) {
+      // Si une erreur se produit, elle est passée à notre action rejetée.
+      return rejectWithValue(err.message);
+    }
+  }
+);
 
 const taskSlice = createSlice({
-    name: "task",
-    initialState: { tasks: [] },
+  name: "task",
+  initialState: {
+    tasks: [],
+    status: "idle",
+    error: null,
+    selectedDate: new Date().toISOString(), // Stockez la date sous forme de chaîne de caractères ISO.
+  },
 
-    reducers: {
-        updateRole(state, action) {
-            state.role = action.payload;
-        },
+  reducers: {
+    setSelectedDate: (state, action) => {
+      // Met à jour la date sélectionnée avec la valeur fournie.
+      state.selectedDate = action.payload;
     },
+    // Ajoutez ici les autres actions de réduction si nécessaire.
+  },
+
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchTasks.pending, (state) => {
+        // Lorsque nous commençons à faire le fetch, nous mettons notre état à 'loading'.
+        state.status = "loading";
+      })
+      .addCase(fetchTasks.fulfilled, (state, action) => {
+        // Lorsque le fetch réussit, nous mettons à jour notre état avec les tâches récupérées et réinitialisons le statut.
+        state.status = "idle";
+        state.tasks = action.payload;
+      })
+      .addCase(fetchTasks.rejected, (state, action) => {
+        // Lorsque le fetch échoue, nous enregistrons l'erreur et réinitialisons le statut.
+        state.status = "idle";
+        state.error = action.error.message;
+      });
+  },
 });
 
 export const taskSliceActions = taskSlice.actions;
