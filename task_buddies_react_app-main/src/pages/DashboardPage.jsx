621abfe1b3c48d88a685188c0e170089e307e31c
Bruce MONGTHE <58480180+BruceMong@users.noreply.github.com>
2023-07-10 00:50:13 +0200

    add DayStat component

--- task_buddies_react_app-main/src/pages/DashboardPage.jsx
+++ task_buddies_react_app-main/src/pages/DashboardPage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import TaskList from "../components/task/TaskList";
 import TaskForm from "../components/task/TaskForm";
+import DayStat from "../components/task/DayStat";
 
 const DashboardPage = () => {
 	return (
 		<div className="dashboardPage">
 			<TaskList />
 			<TaskForm />
+			<DayStat/>
 		</div>
 	);
 };
