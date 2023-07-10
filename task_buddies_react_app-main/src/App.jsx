16de10b4d25f3c05829ec79a11728ff0427676d4
Enzo SALVATI - babi <95984938+babise@users.noreply.github.com>
2023-07-10 09:36:11 +0200

    Add Navbar

--- task_buddies_react_app-main/src/App.jsx
+++ task_buddies_react_app-main/src/App.jsx
@@ -4,15 +4,17 @@ import Routers from "./routes/Router"; // Importez votre fichier Routers
 
 import store from "./store/store";
 import { Provider } from "react-redux";
+import Layout from "./components/Layout";
 
 const App = () => {
 	return (
-	<Provider store={store}>
-		<Router>
-			<Routers />
-		</Router>
-	</Provider>
-
+		<Provider store={store}>
+			<Router>
+				<Layout>
+					<Routers />
+				</Layout>
+			</Router>
+		</Provider>
 	);
-  };
+};
 export default App;
