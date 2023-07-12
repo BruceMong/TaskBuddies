4344ca6e01d9c4cef947cbd4ccb89a46dd975b1e
Enzo SALVATI - babi <95984938+babise@users.noreply.github.com>
2023-07-12 17:56:38 +0200

    Add Tag CSS

--- task_buddies_react_app-main/src/components/tag/TagTile.jsx
+++ task_buddies_react_app-main/src/components/tag/TagTile.jsx
@@ -4,11 +4,17 @@ import { tagIcons } from "../../utils/tagData";
 const TagTile = ({ tag }) => {
 	console.log(tag);
 	return (
-		<div className="tagTile" style={{ color: tag.color }}>
+		<div
+			className="tagTileContainer"
+			style={{
+				color: tag.color,
+				border: `1px solid ${tag.color}`,
+			}}
+		>
 			<div className="tagIcon">
 				<FontAwesomeIcon
 					icon={tagIcons[tag.icon]}
-					style={{ color: tag.color }}
+					style={{ color: tag.color, height: "12px" }}
 				/>
 			</div>
 			<div className="tagTitle">
