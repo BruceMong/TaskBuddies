3ed36fde9c5aec3a8b0e973c22345eaf50e1ac31
Bruce MONGTHE <58480180+BruceMong@users.noreply.github.com>
2023-09-07 15:09:59 +0200

    init

--- task_buddies_react_app-main/src/components/group/GroupTaskFormUpdate.jsx
+++ task_buddies_react_app-main/src/components/group/GroupTaskFormUpdate.jsx
@@ -256,7 +256,6 @@ const TaskFormUpdate = ({ currentTask, groupId, setCurrentTask }) => {
 						value={title}
 						onChange={(event) => setTitle(event.target.value)}
 						placeholder="Titre de la tâche"
-						required
 					/>
 				</div>
 				<div className="inputContainer">
